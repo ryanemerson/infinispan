e9d68a578998fb7b185a9931daae06e05a9717ba
Ryan Emerson <remerson@redhat.com>
2016-11-14 17:20:11 +0000

    ISPN-7149: Create a bridge between the Marshaller interface and Protostuff.

---
+++ client/marshaller/protostuff-marshaller/src/test/java/org/infinispan/marshaller/protostuff/ProtostuffMarshallingTest.java
@@ -0,0 +1,35 @@
+package org.infinispan.marshaller.protostuff;
+
+import org.infinispan.marshaller.test.AbstractMarshallingTest;
+import org.infinispan.marshaller.test.User;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.protostuff.runtime.RuntimeSchema;
+
+/**
+ * @author Ryan Emerson
+ * @since 9.0
+ */
+@Test(groups = "functional", testName = "marshaller.protostuff.ProtostuffMarshallingTest")
+public class ProtostuffMarshallingTest extends AbstractMarshallingTest {
+
+   ProtostuffMarshallingTest() {
+      super(new ProtostuffMarshaller());
+   }
+
+   @BeforeTest
+   public void setup() {
+      RuntimeSchema.register(User.class, new UserSchema());
+   }
+
+   protected void checkCustomSerializerCounters(int readCount, int writeCount) {
+      assert UserSchema.mergeFromCount.get() == readCount;
+      assert UserSchema.writeToCount.get() == writeCount;
+   }
+
+   protected void resetCustomerSerializerCounters() {
+      UserSchema.mergeFromCount.set(0);
+      UserSchema.writeToCount.set(0);
+   }
+}
