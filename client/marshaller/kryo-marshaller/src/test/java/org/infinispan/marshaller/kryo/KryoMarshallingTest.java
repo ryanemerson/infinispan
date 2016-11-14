f120ed3d6b25329f1095f0d1c53ba92524280a71
Ryan Emerson <remerson@redhat.com>
2016-11-14 17:02:27 +0000

    ISPN-7150: Create a bridge between the Marshaller interface and Kryo

---
+++ client/marshaller/kryo-marshaller/src/test/java/org/infinispan/marshaller/kryo/KryoMarshallingTest.java
@@ -0,0 +1,39 @@
+package org.infinispan.marshaller.kryo;
+
+import org.infinispan.marshaller.test.AbstractMarshallingTest;
+import org.infinispan.marshaller.test.User;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.esotericsoftware.kryo.Kryo;
+
+import de.javakaffee.kryoserializers.UnmodifiableCollectionsSerializer;
+
+/**
+ * @author Ryan Emerson
+ * @since 9.0
+ */
+@Test(groups = "functional", testName = "marshaller.kryo.KryoMarshallingTest")
+public class KryoMarshallingTest extends AbstractMarshallingTest {
+
+   KryoMarshallingTest() {
+      super(new KryoMarshaller());
+   }
+
+   @BeforeTest
+   public void setup() {
+      Kryo kryo = ((KryoMarshaller) marshaller).getKryo();
+      kryo.register(User.class, new UserSerializer());
+      UnmodifiableCollectionsSerializer.registerSerializers(kryo);
+   }
+
+   protected void checkCustomSerializerCounters(int readCount, int writeCount) {
+      assert UserSerializer.readCount.get() == readCount;
+      assert UserSerializer.writeCount.get() == writeCount;
+   }
+
+   protected void resetCustomerSerializerCounters() {
+      UserSerializer.readCount.set(0);
+      UserSerializer.writeCount.set(0);
+   }
+}
