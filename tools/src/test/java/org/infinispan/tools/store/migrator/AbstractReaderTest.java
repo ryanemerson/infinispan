fb3b08eb4ee930c174acae27ed1dc722ee3dd3e2
Ryan Emerson <remerson@redhat.com>
2019-02-06 17:49:27 +0000

    StoreMigrator adapted to differentiate between infinispan 8/9/10 marshallers.
    Infinispan 9 marshaller implemented, but still using Externalizers from
    core as they are still needed by the GlobalMarshaller.

--- tools/src/test/java/org/infinispan/tools/store/migrator/AbstractReaderTest.java
+++ tools/src/test/java/org/infinispan/tools/store/migrator/AbstractReaderTest.java
@@ -6,7 +6,7 @@ import static org.infinispan.tools.store.migrator.Element.MARSHALLER;
 import static org.infinispan.tools.store.migrator.Element.SEGMENT_COUNT;
 import static org.infinispan.tools.store.migrator.Element.SOURCE;
 import static org.infinispan.tools.store.migrator.Element.TARGET;
-import static org.infinispan.tools.store.migrator.Element.TYPE;
+import static org.infinispan.tools.store.migrator.Element.VERSION;
 import static org.infinispan.tools.store.migrator.TestUtil.propKey;
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertNotNull;
@@ -14,14 +14,15 @@ import static org.testng.AssertJUnit.assertNotNull;
 import java.util.Properties;
 
 import org.infinispan.Cache;
+import org.infinispan.Version;
 import org.infinispan.configuration.cache.Configuration;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.configuration.global.GlobalConfiguration;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.marshall.core.JBossUserMarshaller;
 import org.infinispan.test.AbstractInfinispanTest;
-import org.infinispan.tools.store.migrator.marshaller.MarshallerType;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 
@@ -30,16 +31,22 @@ public abstract class AbstractReaderTest extends AbstractInfinispanTest {
 
    private static final String TEST_CACHE_NAME = "reader-test";
 
+   protected int majorVersion = 8;
    protected int segmentCount;
 
-   protected <T extends AbstractReaderTest> T segmented(int segmentCount) {
+   public <T extends AbstractReaderTest> T majorVersion(int majorVersion) {
+      this.majorVersion = majorVersion;
+      return (T) this;
+   }
+
+   public <T extends AbstractReaderTest> T segmented(int segmentCount) {
       this.segmentCount = segmentCount;
       return (T) this;
    }
 
    @Override
    protected String parameters() {
-      return "[" + segmentCount + "]";
+      return String.format("[version=%d,segments=%d]", majorVersion, segmentCount);
    }
 
    /**
@@ -56,10 +63,11 @@ public abstract class AbstractReaderTest extends AbstractInfinispanTest {
    }
 
    protected void configureStoreProperties(Properties properties, Element type) {
-      MarshallerType marshallerType = type == SOURCE ? MarshallerType.LEGACY : MarshallerType.CURRENT;
+      // User externalizers must start at JBossUserMarshaller.USER_EXT_ID_MIN in Infinispan 10.x
+      int externalizerId = type == TARGET ? JBossUserMarshaller.USER_EXT_ID_MIN : 256;
       properties.put(propKey(type, CACHE_NAME), TEST_CACHE_NAME);
-      properties.put(propKey(type, MARSHALLER, TYPE), marshallerType.toString());
-      properties.put(propKey(type, MARSHALLER, EXTERNALIZERS), "256:" + TestUtil.TestObjectExternalizer.class.getName());
+      properties.put(propKey(type, MARSHALLER, EXTERNALIZERS), externalizerId + ":" + TestUtil.TestObjectExternalizer.class.getName());
+      properties.put(propKey(type, VERSION), type == SOURCE ? String.valueOf(majorVersion): Version.getMajor());
 
       if (type == TARGET && segmentCount > 0) {
          properties.put(propKey(type, SEGMENT_COUNT), String.valueOf(segmentCount));
@@ -71,11 +79,11 @@ public abstract class AbstractReaderTest extends AbstractInfinispanTest {
       Properties properties = new Properties();
       configureStoreProperties(properties, SOURCE);
       configureStoreProperties(properties, TARGET);
-      // Read from the legacy LevelDB store and populate the new RocksDBStore using latest marshaller
       new StoreMigrator(properties).run();
 
       GlobalConfiguration globalConfig = new GlobalConfigurationBuilder()
-            .serialization().addAdvancedExternalizer(256, new TestUtil.TestObjectExternalizer())
+            .serialization()
+            .addAdvancedExternalizer(JBossUserMarshaller.USER_EXT_ID_MIN, new TestUtil.TestObjectExternalizer())
             .build();
 
       Configuration config = getTargetCacheConfig().build();
