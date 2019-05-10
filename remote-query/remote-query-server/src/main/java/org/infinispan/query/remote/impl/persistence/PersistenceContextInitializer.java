6b19fc872eda83c04931458e68944e374e00166d
Ryan Emerson <remerson@redhat.com>
2019-05-10 16:49:07 +0100

    ISPN-9621 Convert remote-query/server to use protostream

---
+++ remote-query/remote-query-server/src/main/java/org/infinispan/query/remote/impl/persistence/PersistenceContextInitializer.java
@@ -0,0 +1,22 @@
+package org.infinispan.query.remote.impl.persistence;
+
+import org.infinispan.marshall.persistence.impl.PersistenceMarshallerImpl;
+import org.infinispan.protostream.SerializationContextInitializer;
+import org.infinispan.protostream.annotations.AutoProtoSchemaBuilder;
+import org.infinispan.query.remote.impl.indexing.ProtobufValueWrapper;
+
+/**
+ * Interface used to initialise the {@link PersistenceMarshallerImpl}'s {@link org.infinispan.protostream.SerializationContext}
+ * using the specified Pojos, Marshaller implementations and provided .proto schemas.
+ *
+ * @author Ryan Emerson
+ * @since 10.0
+ */
+@AutoProtoSchemaBuilder(
+      autoImportClasses = false,
+      classes = ProtobufValueWrapper.class,
+      schemaFileName = "persistence.remote-query.proto",
+      schemaFilePath = "schema.generated",
+      schemaPackageName = "persistence.remote-query")
+interface PersistenceContextInitializer extends SerializationContextInitializer {
+}
