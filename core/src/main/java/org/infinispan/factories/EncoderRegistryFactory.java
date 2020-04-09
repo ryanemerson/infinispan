6aac54b31ef3f009397531b9ba7dc0d16647454d
Ryan Emerson <remerson@redhat.com>
2020-04-09 17:26:30 +0100

    ISPN-11614 PersistenceMarshaller should utilise an independent SerializationContext for user types
    User marshaller instance now created via MarshallerFactory

--- core/src/main/java/org/infinispan/factories/EncoderRegistryFactory.java
+++ core/src/main/java/org/infinispan/factories/EncoderRegistryFactory.java
@@ -10,6 +10,7 @@ import org.infinispan.commons.dataconversion.IdentityWrapper;
 import org.infinispan.commons.dataconversion.JavaSerializationEncoder;
 import org.infinispan.commons.dataconversion.TranscoderMarshallerAdapter;
 import org.infinispan.commons.dataconversion.UTF8Encoder;
+import org.infinispan.commons.marshall.Marshaller;
 import org.infinispan.commons.marshall.StreamingMarshaller;
 import org.infinispan.encoding.ProtostreamTranscoder;
 import org.infinispan.factories.annotations.ComponentName;
@@ -19,9 +20,6 @@ import org.infinispan.factories.impl.ComponentRef;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.marshall.core.EncoderRegistry;
 import org.infinispan.marshall.core.EncoderRegistryImpl;
-import org.infinispan.marshall.persistence.PersistenceMarshaller;
-import org.infinispan.marshall.persistence.impl.PersistenceContextInitializer;
-import org.infinispan.marshall.protostream.impl.MarshallableUserObject;
 import org.infinispan.marshall.protostream.impl.SerializationContextRegistry;
 
 /**
@@ -34,8 +32,9 @@ public class EncoderRegistryFactory extends AbstractComponentFactory implements 
    // Must not start the global marshaller or it will be too late for modules to register their externalizers
    @Inject @ComponentName(KnownComponentNames.INTERNAL_MARSHALLER)
    ComponentRef<StreamingMarshaller> globalMarshaller;
-   @Inject @ComponentName(KnownComponentNames.PERSISTENCE_MARSHALLER)
-   PersistenceMarshaller persistenceMarshaller;
+   @Inject @ComponentName(KnownComponentNames.USER_MARSHALLER)
+   Marshaller userMarshaller;
+
    @Inject EmbeddedCacheManager embeddedCacheManager;
    @Inject SerializationContextRegistry ctxRegistry;
 
@@ -44,16 +43,13 @@ public class EncoderRegistryFactory extends AbstractComponentFactory implements 
       ClassLoader classLoader = globalConfiguration.classLoader();
       EncoderRegistryImpl encoderRegistry = new EncoderRegistryImpl();
       ClassWhiteList classWhiteList = embeddedCacheManager.getClassWhiteList();
-      // TODO Move registration to GlobalMarshaller ISPN-9622
-      String messageName = PersistenceContextInitializer.getFqTypeName(MarshallableUserObject.class);
-      ctxRegistry.addMarshaller(SerializationContextRegistry.MarshallerType.GLOBAL, new MarshallableUserObject.Marshaller(messageName, persistenceMarshaller.getUserMarshaller()));
 
       encoderRegistry.registerEncoder(IdentityEncoder.INSTANCE);
       encoderRegistry.registerEncoder(UTF8Encoder.INSTANCE);
       encoderRegistry.registerEncoder(new JavaSerializationEncoder(classWhiteList));
       encoderRegistry.registerEncoder(new GlobalMarshallerEncoder(globalMarshaller.wired()));
-      encoderRegistry.registerTranscoder(new DefaultTranscoder(persistenceMarshaller.getUserMarshaller()));
-      encoderRegistry.registerTranscoder(new BinaryTranscoder(persistenceMarshaller.getUserMarshaller()));
+      encoderRegistry.registerTranscoder(new DefaultTranscoder(userMarshaller));
+      encoderRegistry.registerTranscoder(new BinaryTranscoder(userMarshaller));
       // Wraps the GlobalMarshaller so that it can be used as a transcoder
       encoderRegistry.registerTranscoder(new TranscoderMarshallerAdapter(globalMarshaller.wired()));
       encoderRegistry.registerTranscoder(new ProtostreamTranscoder(ctxRegistry, classLoader));
--- core/src/main/java/org/infinispan/factories/MarshallerFactory.java
+++ core/src/main/java/org/infinispan/factories/MarshallerFactory.java
@@ -1,11 +1,24 @@
 package org.infinispan.factories;
 
+import static org.infinispan.util.logging.Log.CONFIG;
+
+import java.lang.reflect.Method;
+
+import org.infinispan.commons.marshall.ImmutableProtoStreamMarshaller;
+import org.infinispan.commons.marshall.Marshaller;
 import org.infinispan.commons.marshall.StreamAwareMarshaller;
 import org.infinispan.commons.marshall.StreamingMarshaller;
+import org.infinispan.commons.util.ReflectionUtil;
+import org.infinispan.commons.util.Util;
+import org.infinispan.configuration.global.SerializationConfiguration;
 import org.infinispan.factories.annotations.DefaultFactoryFor;
+import org.infinispan.factories.annotations.Inject;
 import org.infinispan.factories.impl.ComponentAlias;
+import org.infinispan.factories.impl.ComponentRef;
 import org.infinispan.marshall.core.GlobalMarshaller;
+import org.infinispan.marshall.core.impl.DelegatingUserMarshaller;
 import org.infinispan.marshall.persistence.impl.PersistenceMarshallerImpl;
+import org.infinispan.marshall.protostream.impl.SerializationContextRegistry;
 
 /**
  * MarshallerFactory.
@@ -13,9 +26,22 @@ import org.infinispan.marshall.persistence.impl.PersistenceMarshallerImpl;
  * @author Galder Zamarreño
  * @since 4.0
  */
-@DefaultFactoryFor(classes = {StreamingMarshaller.class, StreamAwareMarshaller.class},
-      names = {KnownComponentNames.INTERNAL_MARSHALLER, KnownComponentNames.PERSISTENCE_MARSHALLER})
+@DefaultFactoryFor(
+      classes = {
+            Marshaller.class,
+            StreamingMarshaller.class,
+            StreamAwareMarshaller.class
+      },
+      names = {
+            KnownComponentNames.INTERNAL_MARSHALLER,
+            KnownComponentNames.PERSISTENCE_MARSHALLER,
+            KnownComponentNames.USER_MARSHALLER
+      }
+)
 public class MarshallerFactory extends AbstractComponentFactory implements AutoInstantiableFactory {
+
+   @Inject
+   ComponentRef<SerializationContextRegistry> contextRegistry;
 
    @Override
    public Object construct(String componentName) {
@@ -29,8 +55,31 @@ public class MarshallerFactory extends AbstractComponentFactory implements AutoI
             return new PersistenceMarshallerImpl();
          case KnownComponentNames.INTERNAL_MARSHALLER:
             return new GlobalMarshaller();
+         case KnownComponentNames.USER_MARSHALLER:
+            return createUserMarshaller();
          default:
             throw new IllegalArgumentException(String.format("Marshaller name '%s' not recognised", componentName));
       }
+   }
+
+   private Marshaller createUserMarshaller() {
+      SerializationConfiguration serializationConfig = globalConfiguration.serialization();
+      Marshaller userMarshaller = serializationConfig.marshaller();
+      if (userMarshaller != null) {
+         Class<? extends Marshaller> clazz = userMarshaller.getClass();
+         if (clazz.getName().equals(Util.JBOSS_USER_MARSHALLER_CLASS)) {
+            // If the user has specified to use jboss-marshalling, we must initialize the instance with the GlobalComponentRegistry
+            // So that any user Externalizer implementations can be loaded.
+            Method method = ReflectionUtil.findMethod(clazz, "initialize", GlobalComponentRegistry.class);
+            ReflectionUtil.invokeAccessibly(userMarshaller, method, globalComponentRegistry);
+            CONFIG.jbossMarshallingDetected();
+            return new DelegatingUserMarshaller(userMarshaller);
+         }
+      } else {
+         userMarshaller = new ImmutableProtoStreamMarshaller(contextRegistry.wired().getUserCtx());
+      }
+
+      userMarshaller.initialize(globalComponentRegistry.getCacheManager().getClassWhiteList());
+      return new DelegatingUserMarshaller(userMarshaller);
    }
 }
