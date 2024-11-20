98fcca9993d849b9314e5ac4f1b397a5e5a371e1
Ryan Emerson <remerson@redhat.com>
2024-11-20 16:35:53 +0000

    More xsite test fixes

--- core/src/main/java/org/infinispan/topology/RebalancingStatus.java
+++ core/src/main/java/org/infinispan/topology/RebalancingStatus.java
@@ -1,7 +1,7 @@
 package org.infinispan.topology;
 
 import org.infinispan.commons.marshall.ProtoStreamTypeIds;
-import org.infinispan.protostream.annotations.ProtoEnumValue;
+import org.infinispan.protostream.annotations.Proto;
 import org.infinispan.protostream.annotations.ProtoTypeId;
 
 /**
@@ -11,13 +11,10 @@ import org.infinispan.protostream.annotations.ProtoTypeId;
  * @since 8.1
  */
 @ProtoTypeId(ProtoStreamTypeIds.REBALANCE_STATUS)
+@Proto
 public enum RebalancingStatus {
-   @ProtoEnumValue(number = 1)
    SUSPENDED,
-   @ProtoEnumValue(number = 2)
    PENDING,
-   @ProtoEnumValue(number = 3)
    IN_PROGRESS,
-   @ProtoEnumValue(number = 4)
    COMPLETE
 }
