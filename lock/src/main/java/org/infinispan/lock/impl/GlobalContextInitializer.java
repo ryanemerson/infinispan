bb9d458d32aa8bce775d69c330a70b4db9fa6db9
Ryan Emerson <remerson@redhat.com>
2024-11-26 12:15:55 +0000

    Lock module converted and all tests passing

---
+++ lock/src/main/java/org/infinispan/lock/impl/GlobalContextInitializer.java
@@ -0,0 +1,23 @@
+package org.infinispan.lock.impl;
+
+import org.infinispan.protostream.SerializationContextInitializer;
+import org.infinispan.protostream.annotations.ProtoSchema;
+
+@ProtoSchema(
+      dependsOn = org.infinispan.marshall.persistence.impl.PersistenceContextInitializer.class,
+      includeClasses = {
+            org.infinispan.lock.impl.entries.ClusteredLockKey.class,
+            org.infinispan.lock.impl.entries.ClusteredLockValue.class,
+            org.infinispan.lock.impl.entries.ClusteredLockState.class,
+            org.infinispan.lock.impl.functions.IsLocked.class,
+            org.infinispan.lock.impl.functions.LockFunction.class,
+            org.infinispan.lock.impl.functions.UnlockFunction.class,
+            org.infinispan.lock.impl.lock.ClusteredLockFilter.class
+      },
+      schemaFileName = "global.lock.proto",
+      schemaFilePath = "proto/generated",
+      schemaPackageName = "org.infinispan.global.lock",
+      service = false
+)
+interface GlobalContextInitializer extends SerializationContextInitializer {
+}
